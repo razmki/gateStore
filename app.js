f4a2677858fe8db584b98ccc0dab3720531811e9
razmki <swatijke1@gmail.com>
2016-08-20 17:15:41 +0300

    firstUp

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var app = angular.module('gateStore', []);
+
+app.controller("CalculateController", function(){
+ 		this.products = prices;
+      	this.changed = function() {
+     	return this.products.gate;
+      }
+      	this.changed1 = function() {
+      	this.products.openes;
+      }
+      this.changed3 = function() {
+      this.products.gate;
+      }
+      this.changedHeight = function() {
+      this.products.height;
+      }
+});
+app.controller("SumController", function(){
+	
+});
+
+var prices = 
+	{
+		"roles" : 1,
+   		"openes" : 2,
+		"sewingUp" : 3,
+		"Combine" : 4,
+		"stepOn" : 5,
+		"width" : 0.01,
+		"height" : 0.02, 
+		"yourWorld": 6,
+		"client" : 7,
+		"gate" : 8,
+		"automate" : 9,
+		"bonds" : 10,
+		"rack" : 11,
+		"sewing" : 12,
+		"mounting" : 13,
+		"concrete" : 14
+	};
